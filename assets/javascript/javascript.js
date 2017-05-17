6289f5c309cf170b9a41970ff268c166b57848f9
Krista Lane <klane@calvisitor-10-105-175-207.calvisitor.1918.berkeley.edu>
2017-05-16 20:19:27 -0700

    testing

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,13 @@
+
+
+// determining if game is running
+document.onkeypress = keyPressed;
+  function keyPressed(event) {
+    if(isGameRunning){
+      playGame(event)
+    }
+    else{
+      resetGame()
+      // need to identify the right classes, then .empty()
+    }
+  }
